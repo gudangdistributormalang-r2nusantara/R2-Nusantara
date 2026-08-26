d1e2470e5dabab70bf235a1e30a90c2a924f3e3d
R2 NUSANTARA <gudangdistributormalang@gmail.com>
2026-08-26 10:41:16 +0700

    Delete js/config.js

--- js/config.js
+++
@@ -1,8 +0,0 @@
-// Konfigurasi Global
-export const CONFIG = {
-  ITEMS_PER_PAGE: 8,        // 4 baris × 2 kolom
-  DEFAULT_CATALOG: 'r2',
-  DEFAULT_FILTER: 'all',
-  DEFAULT_SORT: 'name-asc',
-  FREE_SHIPPING_THRESHOLD: 20, // slop
-};
